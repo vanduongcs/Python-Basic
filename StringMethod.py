8a732694852a2b7f1f3cc61bb59f9937d4aca641
vanduongcs <vanduongcomsci@gmail.com>
2024-10-12 00:30:37 +0700

    Update StringMethod.py

--- StringMethod.py
+++ StringMethod.py
@@ -16,6 +16,19 @@ print(f"This is the result of casefold() method: \"{result}\"")
 result = stringTest.strip()
 print(f"This is the result of strip() method: \"{result}\"")
 
-# lstrip() method remove all of space int the start position of string
+# lstrip() method remove all of space in the start position of string
+# if you need to remove all of space in the end position of string, you must use the rstrip() method
 result = stringTest.lstrip()
 print(f"This is the result of lstrip() method: \"{result}\"")
+
+# find(<specify string>,[start],[end]) method result the first position of specify string in the string
+# [start] and [end] is optional
+result = stringTest.find("is",4,13)
+print(f"This is the result of find() method: \"{result}\"")
+
+# Another method same to the find() method is index()
+# A big different of them is that: if find() method can not find the specify string position int the string, it will return -1
+# But in this situation, the index() method will notify an error
+
+result = stringTest.index("can not find this string")
+print(f"This is the result of index() method: \"{result}\"")
