3d29edb31f1b96c4c35a9a63eee5bc4ad258b9fb
vanduongcs <vanduongcomsci@gmail.com>
2024-10-11 23:48:25 +0700

    Update StringMethod.py

--- StringMethod.py
+++ StringMethod.py
@@ -1,13 +1,21 @@
-stringTest = "  This is a SIMPLE TESt    "
+stringTest = "  This is a SIMPLE TESt and number: 1 2 3, and α  "
 
-#upper() method convert all of the characters in the string from normal to upper case
+# upper() method convert all of the characters in the string from normal to upper case
 result = stringTest.upper()
 print(f"This is the result of upper() method: \"{result}\"")
 
-#lower() method convert all of the characters in the string from upper case to lower case
+# lower() method convert all of the characters in the string from upper case to lower case
 result = stringTest.lower()
 print(f"This is the result of lower() method: \"{result}\"")
 
-#Same to the lower() method, but it's stronger than lower case because it can convert character outside the alphabet
+# Same to the lower() method, but it's stronger than lower case because it can convert character outside the alphabet
 result = stringTest.casefold()
+print(f"This is the result of casefold() method: \"{result}\"")
 
+# strip() method remove all of space in the start position and end position of string 
+result = stringTest.strip()
+print(f"This is the result of strip() method: \"{result}\"")
+
+# lstrip() method remove all of space int the start position of string
+result = stringTest.lstrip()
+print(f"This is the result of lstrip() method: \"{result}\"")
