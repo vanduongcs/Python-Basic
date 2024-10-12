da3e1a61b38e6755fa8e90810a78208e3952422b
vanduongcs <vanduongcomsci@gmail.com>
2024-10-12 09:34:03 +0700

    Practice

--- Practice-String/StringReversal.py
+++ Practice-String/StringReversal.py
@@ -1,17 +1,20 @@
-# Input the string
+# Method 1: Using the build-in method in python
 print("The first method is that: ")
 string = input("Please press any string: ")
 
-#Method 1: Using the build-in method in python
 # This method concatenate the reversal string after init string
 result = "".join(reversed(string))
-
 print(f"The string after reverse: {result}")
 
 #Method 2: Using string slicing
 print("\nThe second method is that: ")
 secondString = input("Please press any string: ")
 
-result = secondString[::-1]
+'''
+ This method concatenate the string with that means: Start from zero and end at string end position by the step with value: -1
+    For example: Step = 1 will act like here: position: 0 -> 1 -> 2 -> 3
+    More example: Step = -1 will act like here: position: -1 -> -2 -> -3 -> -4
+'''
 
+result = secondString[::-1]
 print(f"The string after reverse: {result}")
